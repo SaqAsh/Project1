7897f0dce7af36aea10cb82afe754404104be80c
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2025-01-15 00:19:46 -0500

    changed up the input handler so it uses a pointer to the Time series object instead of the object itself on the stack, which allows for the manipulation of the pointers to be better

--- InputHandler.cpp
+++ InputHandler.cpp
@@ -2,49 +2,50 @@
 
 #include <string>
 #include "TimeSeries.h"  
-void HandleLoad (bool &first_load, TimeSeries& t_series){
+void HandleLoad (bool first_load, TimeSeries *&t_series){
     std::string file_name;
     if(std::cin>> file_name){
         if (first_load) {
             first_load = false;
         } else {
-            t_series= TimeSeries();  
+            delete t_series;
+            t_series = new TimeSeries();  
         }
     }
 
-    t_series.LOAD(file_name);
+    t_series->LOAD(file_name);
     std::cout << "success" << "\n";
 }
 
-void HandleAdd(TimeSeries &t_series){
+void HandleAdd(TimeSeries *t_series){
     unsigned int Y;
     int D;
     if (std::cin >> Y >> D) {
-        t_series.ADD(Y, D);
+        t_series->ADD(Y, D);
     }
 }
 
-void HandleUpdate(TimeSeries &t_series){
+void HandleUpdate(TimeSeries *t_series){
     unsigned int Y;
     int D;
     if (std::cin >> Y >> D) {
-        t_series.UPDATE(Y, D);
+        t_series->UPDATE(Y, D);
     }
 }
 
-void HandleMean(TimeSeries &t_series){
-    double m = t_series.mean();
+void HandleMean(TimeSeries *t_series){
+    double m = t_series->mean();
     !m ? std::cout<<"failure" << "\n" : std::cout << "mean is " << m <<"\n";
 }
 
-void HandleMonotonic(TimeSeries &t_series){
-    t_series.is_monotonic() ? std::cout << "series is monotonic"<<"\n" : std::cout << "series is not monotonic"<<"\n"; 
+void HandleMonotonic(TimeSeries *t_series){
+    t_series->is_monotonic() ? std::cout << "series is monotonic"<<"\n" : std::cout << "series is not monotonic"<<"\n"; 
 }
 
-void HandleFit(TimeSeries &t_series){
+void HandleFit(TimeSeries *t_series){
     double slope, intercept;
 
-    t_series.best_fit(slope, intercept);
+    t_series->best_fit(slope, intercept);
 
     if (slope == 0 && intercept == 0){
         std::cout<<"failure" << "\n";
@@ -56,7 +57,7 @@ void HandleFit(TimeSeries &t_series){
 int main() {
 
     bool first_load = true;  
-    TimeSeries time_series;          
+    TimeSeries *time_series = new TimeSeries();          
 
     std::string command;
 
@@ -71,7 +72,7 @@ int main() {
             HandleLoad(first_load, time_series);
         }
         else if (command == "PRINT") {
-            time_series.PRINT();
+            time_series->PRINT();
         }
         else if (command == "ADD") {
             HandleAdd(time_series);
